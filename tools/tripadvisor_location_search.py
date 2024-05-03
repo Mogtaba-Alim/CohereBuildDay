99ff0a0e6233a6501200657a2016ca0235a81f7b
rafael madrigal <rfgmadrigal@gmail.com>
2024-05-03 18:50:22 -0400

    commit

--- tools/tripadvisor_location_search.py
+++ tools/tripadvisor_location_search.py
@@ -1,12 +1,10 @@
 import requests
 import cohere
-import pandas as pd
-import numpy as np
-import altair.expr as alt
-from sklearn.decomposition import PCA
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.cluster import KMeans
 
+from sklearn.metrics.pairwise import cosine_similarity
+
+import requests
+from langchain.tools import tool
 
 
 co = cohere.Client("URKBopZalYu3WeoBZtmoFm66f3Cnuu7MkLWYyq6a") 
@@ -31,6 +29,36 @@ def string_to_query_string(s):
     # Replace spaces with '%20'
     query_string = s.replace(' ', '%20')
     return query_string
+
+
+    
+    # Get text embeddings
+def get_embeddings(texts, model='embed-english-v3.0', input_type="search_document"):
+    output = co.embed(
+        model=model,
+        input_type=input_type,
+        texts=texts)
+    return output.embeddings
+
+def cosine_similarity(vector1, vector2):
+    """
+    Compute cosine similarity between two vectors.
+
+    Args:
+    vector1 (numpy array): The first vector.
+    vector2 (numpy array): The second vector.
+
+    Returns:
+    float: Cosine similarity between the two vectors.
+    """
+    vector1 = np.squeeze(vector1)
+    vector2 = np.squeeze(vector2)
+    
+    dot_product = np.dot(vector1, vector2)
+    norm1 = np.linalg.norm(vector1)
+    norm2 = np.linalg.norm(vector2)
+    similarity = dot_product / (norm1 * norm2)
+    return similarity
 
 def get_top_locations(query, location_name, category, num_results=3):
     # Fetch data from TripAdvisor API
@@ -67,32 +95,8 @@ def get_top_locations(query, location_name, category, num_results=3):
         
     else:
         return None
-
     
-    # Get text embeddings
-def get_embeddings(texts, model='embed-english-v3.0', input_type="search_document"):
-    output = co.embed(
-        model=model,
-        input_type=input_type,
-        texts=texts)
-    return output.embeddings
-
-def cosine_similarity(vector1, vector2):
-    """
-    Compute cosine similarity between two vectors.
-
-    Args:
-    vector1 (numpy array): The first vector.
-    vector2 (numpy array): The second vector.
-
-    Returns:
-    float: Cosine similarity between the two vectors.
-    """
-    vector1 = np.squeeze(vector1)
-    vector2 = np.squeeze(vector2)
-    
-    dot_product = np.dot(vector1, vector2)
-    norm1 = np.linalg.norm(vector1)
-    norm2 = np.linalg.norm(vector2)
-    similarity = dot_product / (norm1 * norm2)
-    return similarity
+@tool
+def location_search(query, location_name, category) -> list:
+    "This tool is only used to find locations on tripadvisor that falls in a specific `category` and `location_name` provided given a user `query`. Do not use it for anything else. `category` is any of the following: hotels, attractions, restaurants"
+    return get_top_locations(query, location_name, category)
